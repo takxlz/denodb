d1b06c8d06e4de454f9bdcf2ac869ff5e55c004a
takxlz <tak.root.yk1012@gmail.com>
2021-03-18 09:35:09 +0900

    test: change dex url

--- deps.ts
+++ deps.ts
@@ -1,6 +1,7 @@
 export * as ConsoleColor from "https://deno.land/x/colorlog@v1.0/mod.ts";
 
-export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/denjucks/dex/master/mod.ts";
+// export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/denjucks/dex/master/mod.ts";
+export { default as SQLQueryBuilder } from "https://deno.land/x/dex/mod.ts";
 
 export { camelCase, snakeCase } from "https://deno.land/x/case/mod.ts";
 
