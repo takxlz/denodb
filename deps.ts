9138177f6f522057e2122197a6f0644fc2fa191c
takxlz <tak.root.yk1012@gmail.com>
2021-03-20 00:55:17 +0900

    fix

--- deps.ts
+++ deps.ts
@@ -2,7 +2,8 @@ export * as ConsoleColor from "https://deno.land/x/colorlog@v1.0/mod.ts";
 
 // export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/denjucks/dex/master/mod.ts";
 // export { default as SQLQueryBuilder } from "https://deno.land/x/dex/mod.ts";
-export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/takxlz/dex/master/mod.ts";
+// export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/takxlz/dex/master/mod.ts";
+export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/aghussb/dex/master/mod.ts";
 
 export { camelCase, snakeCase } from "https://deno.land/x/case/mod.ts";
 
